5d9af0673a7e6e63617f9103a78322b16591830d
Marimuthuc17 <kishorecmm@gmail.com>
2026-05-30 09:31:42 +0530

    First Time Push

---
+++ src/test/java/com/omrbranch/git/Sample1.java
@@ -0,0 +1,24 @@
+package com.omrbranch.git;
+
+public class Sample1 {
+	
+	public void method1() {
+		
+		System.out.println("method1");
+
+	}
+	
+public void method2() {
+		
+		System.out.println("method2");
+
+	}
+
+public static void main(String[] args) {
+	
+	Sample1 sample = new Sample1();
+	sample.method1();
+	sample.method2();
+	
+  }
+}
